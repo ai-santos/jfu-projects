42932b673faf3f7679803d020bd11a60d7ffc708
Aileen <asantos3026@gmail.com>
2017-03-03 13:11:22 -0800

    Template works, underscore works, ui works

--- dist/public/scripts/app.js
+++ dist/public/scripts/app.js
@@ -4,13 +4,31 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log('the dom has loaded');
 });
 
-var data = {
-  project_name: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-  phone: "",
-  contact: "",
-  email: ""
-};
+var projects = [{
+  project_name: "proj1", address: "add1", city: "city1", state: "state1", zip: "12345",
+  phone: "phone1", contact: "contact1", email: "email1"
+}, {
+  project_name: "proj2", address: "add2", city: "city2", state: "state2", zip: "12345", phone: "5105757589",
+  contact: "", email: ""
+}, {
+  project_name: "proj3", address: "add2", city: "city3", state: "state3", zip: "12345", phone: "5105757589",
+  contact: "", email: ""
+}, {
+  project_name: "proj4", address: "add2", city: "city4", state: "state4", zip: "12345", phone: "5105757589",
+  contact: "", email: ""
+}, {
+  project_name: "proj5", address: "add2", city: "city5", state: "state5", zip: "12345", phone: "5105757589",
+  contact: "", email: ""
+}, {
+  project_name: 'proj6', address: "add2", city: "city6", state: "state6", zip: "12345", phone: "5105757589",
+  contact: "", email: ""
+}];
+
+var projTemplate = _.template('<% _.each(projects, function(project, index, projects) { %>' + '<div class="col-md-3">' + '<h5><%= project.project_name %></h5>' + '<h5><%= project.address %></h5>' + '<h5><%= project.city %></h5>' + '<h5><%= project.state %></h5>' + '<h5><%= project.zip %></h5>' + '<h5><%= project.phone %></h5>' + '<h5><%= project.contact %></h5>' + '<h5><%= project.email %></h5>' + '</div>' + '<% }); %>');
+
+var content = projTemplate({
+  projects: projects
+});
+
+var proj_container = document.querySelector('.projects');
+proj_container.innerHTML = content;
