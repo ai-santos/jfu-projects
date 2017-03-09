728dc9b3b7114bd17afa93fd5acda0fd0ab918df
Aileen <asantos3026@gmail.com>
2017-03-08 16:57:58 -0800

    WIP map now dynamic

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,16 +1,14 @@
 $(document).ready( () => {
-  // $.ajax({
-  //   url: 'http://localhost:8080/projects',
-  //   success: (result) => {
 
-  //   }
-  // })
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhbnRvczMwMjYiLCJhIjoiZWZlMmMyM2JiN2ZiNzcxZmJkOGJhMWNhZWE4ODc1MjMifQ.Moj73Bv5_uyylRIcZkXcYg';
+
+  const map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/asantos3026/cj01ninbe00012rml9jjb8bjj', //stylesheet location
+      center: [-122.208515, 37.779505], // starting position
+      zoom: 9 // starting zoom
+  });
+
 })
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   console.log('the dom has loaded')
-// })
-
-
 
 
