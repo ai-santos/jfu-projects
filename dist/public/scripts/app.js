af57a713bb8ad6084f2642ba3d47bdb3d5e6a0f2
Aileen <asantos3026@gmail.com>
2017-03-11 12:08:17 -0800

    Switch API to open layers

--- dist/public/scripts/app.js
+++ dist/public/scripts/app.js
@@ -1,41 +1,16 @@
 'use strict';
 
 $(document).ready(function () {
-
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhbnRvczMwMjYiLCJhIjoiZWZlMmMyM2JiN2ZiNzcxZmJkOGJhMWNhZWE4ODc1MjMifQ.Moj73Bv5_uyylRIcZkXcYg';
-
-  var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v10', //stylesheet location
-    center: [-122.208515, 37.779505], // starting position
-    zoom: 9 // starting zoom
+  var map = new ol.Map({
+    target: 'map',
+    layers: [new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })],
+    view: new ol.View({
+      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 10
+    })
   });
 
-  var geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-
-  });
-
-  document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-  // map.addControl(new mapboxgl.NavigationControl());
-  var getAllMyProjects = function getAllMyProjects(serialData) {
-    $.get('/api/projects', function (data) {
-      var allProjects = data;
-      // console.log(data)
-
-      for (var index in data) {
-        var address = data[index].address.toString();
-        var state = data[index].state.toString();
-        var city = data[index].city.toString();
-        var zip = data[index].zip.toString();
-
-        var entireAddress = [address + ' ' + state + ' ' + city + ',' + zip];
-        console.log(entireAddress.join(''));
-        // geocoder.query(entireAddress, (err, result) => {
-        //   console.log('this is our result--->', result)
-        // })
-      }
-    });
-  };
-  getAllMyProjects();
+  map.getView().setCenter(ol.proj.transform([-122.208515, 37.779505], 'EPSG:4326', 'EPSG:3857'));
 });
--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,41 +1,18 @@
 $(document).ready( () => {
-
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhbnRvczMwMjYiLCJhIjoiZWZlMmMyM2JiN2ZiNzcxZmJkOGJhMWNhZWE4ODc1MjMifQ.Moj73Bv5_uyylRIcZkXcYg';
-
-  const map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v10', //stylesheet location
-      center: [-122.208515, 37.779505], // starting position
-      zoom: 9 // starting zoom
-  });
-
-  const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-
-  });
-
-  document.getElementById('geocoder').appendChild(geocoder.onAdd(map))
-  // map.addControl(new mapboxgl.NavigationControl());
-  const getAllMyProjects = (serialData) => {
-    $.get('/api/projects', (data) => {
-        let allProjects = data
-        // console.log(data)
-
-        for(let index in data) {
-          let address = data[index].address.toString()
-          let state = data[index].state.toString()
-          let city = data[index].city.toString()
-          let zip = data[index].zip.toString()
-
-          let entireAddress = [address + ' ' + state + ' ' + city + ',' +zip]
-          console.log(entireAddress.join(''))
-          // geocoder.query(entireAddress, (err, result) => {
-          //   console.log('this is our result--->', result)
-          // })
-        }
+    const map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 10
+      })
     })
-  }
-  getAllMyProjects()
+
+    map.getView().setCenter(ol.proj.transform([-122.208515, 37.779505], 'EPSG:4326', 'EPSG:3857'))
 })
 
 
